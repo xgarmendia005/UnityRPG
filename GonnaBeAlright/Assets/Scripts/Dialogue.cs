fa2035cea0f9d924675dff953156b5fe2e5b06f2
Xabier Garmendia <45872309+xgarmendia005@users.noreply.github.com>
2021-12-03 14:59:46 +0100

    Introduction house set up

---
+++ GonnaBeAlright/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextAsset textFile;
+    public int currentSpeaker;
+    private string[] sentences;
+    private string[] currentSentence;
+
+    public GameObject dialogueBox;
+    public Text nameDisplay;
+    public Text textDisplay;
+    public int index;
+    public float typingSpeed;
+
+    private void Start()
+    {
+        sentences = textFile.text.Split('\n');
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        currentSentence = sentences[index].Split('-');
+
+        currentSpeaker = int.Parse(currentSentence[0]);
+
+        nameDisplay.text = currentSentence[1];
+
+        foreach (char letter in currentSentence[2].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence ()
+    {
+        if (textDisplay.text == currentSentence[2])
+        {
+            if (index < sentences.Length - 1)
+            {
+                index++;
+                textDisplay.text = "";
+                StartCoroutine(Type());
+            }
+            else
+            {
+                textDisplay.text = "";
+            }
+        }
+    }
+
+    public void HideDialogue ()
+    {
+        if (textDisplay.text == currentSentence[2])
+        {
+            dialogueBox.SetActive(false);
+        } 
+    }
+
+    public void ShowDialogue ()
+    {
+        NextSentence();
+        dialogueBox.SetActive(true);
+    }
+}
---
+++ GonnaBeAlright/Assets/Scripts/Introduction.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour
+{
+
+    bool inDialogue = true;
+    public Dialogue dialogueManager;
+
+    public string[] characters;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && inDialogue)
+        {
+            //Healer gets to door
+            if (dialogueManager.index == 23)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Healer leaves house and returns
+            else if(dialogueManager.index == 32)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Scream outside
+            else if (dialogueManager.index == 36)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Hero leaves house
+            else if (dialogueManager.index == 38)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Healer leaves house
+            else if (dialogueManager.index == 39)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Healer approaches hero and woman
+            else if (dialogueManager.index == 45)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Glitch appears
+            else if (dialogueManager.index == 47)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Woman disappears
+            else if (dialogueManager.index == 51)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Hero starts to go away
+            else if (dialogueManager.index == 61)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Short scene: Glitch takes hero, healer runs without thinking
+            else if (dialogueManager.index == 62)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Hero disappears. Dialogue box falls to the ground
+            else if (dialogueManager.index == 63)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Healer takes Hero text from dialogue box and put it in hers
+            else if (dialogueManager.index == 64)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            //Hero changes
+            else if (dialogueManager.index == 65)
+            {
+                dialogueManager.HideDialogue();
+                inDialogue = false;
+            }
+            else
+            {
+                dialogueManager.NextSentence();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1) && !inDialogue)
+        {
+            inDialogue = true;
+            dialogueManager.ShowDialogue();
+        }
+
+    }
+}
