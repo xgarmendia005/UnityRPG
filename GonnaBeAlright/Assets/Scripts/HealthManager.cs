55a74fee946d21c9e7ed5fcf890565eddeded300
Xabier Garmendia <45872309+xgarmendia005@users.noreply.github.com>
2022-01-13 12:16:38 +0100

    Dynamic turn order and death

--- GonnaBeAlright/Assets/Scripts/HealthManager.cs
+++ GonnaBeAlright/Assets/Scripts/HealthManager.cs
@@ -7,15 +7,25 @@ public class HealthManager : MonoBehaviour
     public int maxHealth;
     private int currentHealth;
 
+    public bool dead = false;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
     }
 
-    public void ModifyHealth (int change)
+    public bool ModifyHealth (int change)
     {
         currentHealth += change;
         print(transform.name + ": " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            dead = true;
+            return true;
+        }
+
+        return false;
     }
 }
--- GonnaBeAlright/Assets/Scripts/RPGBattle.cs
+++ GonnaBeAlright/Assets/Scripts/RPGBattle.cs
@@ -6,7 +6,12 @@ public class RPGBattle : MonoBehaviour
 {
     public GameObject[] characters;
     private int curChar = 0;
-    private int[] enemyIndex;
+    private GameObject tempChar;
+    private List<int> orderIndex;
+    private List<int> playerIndex;
+    private List<int> enemyIndex;
+
+    private int curTarget;
 
     private const int BATTLESTART = 0;
     private const int TURNSTART = 1;
@@ -16,7 +21,9 @@ public class RPGBattle : MonoBehaviour
 
     void Start()
     {
-        enemyIndex = new int[] { 4, 5, 6, 7 };
+        orderIndex = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+        playerIndex = new List<int> { 0, 1, 2, 3 };
+        enemyIndex = new List<int> { 4, 5, 6, 7 };
     }
 
     public void Update()
@@ -36,39 +43,89 @@ public class RPGBattle : MonoBehaviour
                 battleState = TURNSTART;
                 break;
             case TURNSTART:
-                characters[curChar].GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 1f);
+                tempChar = characters[orderIndex[curChar]];
+                tempChar.GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 1f);
                 battleState = TARGETSEL;
                 break;
             case TARGETSEL:
-                if (Input.GetKeyDown(KeyCode.Alpha1))
+                if (tempChar.GetComponent <Stats>().player){
+                    if (Input.GetKeyDown(KeyCode.Alpha1))
+                    {
+                        curTarget = enemyIndex[0];
+                        if (characters[curTarget].GetComponent<HealthManager>().ModifyHealth(-15))
+                        {
+                            KillCharacter(curTarget);
+                        }
+                        tempChar.GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
+                        curChar++;
+                        battleState = TURNSTART;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha2))
+                    {
+                        curTarget = enemyIndex[1];
+                        if (characters[curTarget].GetComponent<HealthManager>().ModifyHealth(-15))
+                        {
+                            KillCharacter(curTarget);
+                        }
+                        tempChar.GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
+                        curChar++;
+                        battleState = TURNSTART;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha3))
+                    {
+                        curTarget = enemyIndex[2];
+                        if (characters[curTarget].GetComponent<HealthManager>().ModifyHealth(-15))
+                        {
+                            KillCharacter(curTarget);
+                        }
+                        tempChar.GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
+                        curChar++;
+                        battleState = TURNSTART;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Alpha4))
+                    {
+                        curTarget = enemyIndex[3];
+                        if (characters[curTarget].GetComponent<HealthManager>().ModifyHealth(-15))
+                        {
+                            KillCharacter(curTarget);
+                        }
+                        tempChar.GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
+                        curChar++;
+                        battleState = TURNSTART;
+                    }
+                    
+                }
+                else
                 {
-                    characters[enemyIndex[0]].GetComponent<HealthManager>().ModifyHealth(-15);
-                    characters[curChar].GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
+                    characters[playerIndex[Random.Range(0, playerIndex.Count)]].GetComponent<HealthManager>().ModifyHealth(-15);
+                    tempChar.GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
                     curChar++;
                     battleState = TURNSTART;
                 }
-                else if (Input.GetKeyDown(KeyCode.Alpha2))
-                {
-                    characters[enemyIndex[1]].GetComponent<HealthManager>().ModifyHealth(-15);
-                    characters[curChar].GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
-                    curChar++;
-                    battleState = TURNSTART;
-                }
-                else if (Input.GetKeyDown(KeyCode.Alpha3))
-                {
-                    characters[enemyIndex[2]].GetComponent<HealthManager>().ModifyHealth(-15);
-                    characters[curChar].GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
-                    curChar++;
-                    battleState = TURNSTART;
-                }
-                else if (Input.GetKeyDown(KeyCode.Alpha4))
-                {
-                    characters[enemyIndex[3]].GetComponent<HealthManager>().ModifyHealth(-15);
-                    characters[curChar].GetComponent<Renderer>().material.SetFloat("_OutlineAlpha", 0f);
-                    curChar++;
-                    battleState = TURNSTART;
-                }
+                if (curChar >= orderIndex.Count) curChar = 0;
                 break;
         }
     }
+
+    public void CalculateIndex()
+    {
+        playerIndex = new List<int>();
+        enemyIndex = new List<int>();
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (!characters[i].GetComponent<HealthManager>().dead){
+                if (characters[i].GetComponent<Stats>().player) playerIndex.Add(i);
+                else enemyIndex.Add(i);
+            }
+        }
+    }
+
+    public void KillCharacter (int i)
+    {
+        if (orderIndex.IndexOf(curTarget) <= curChar) curChar--;
+        orderIndex.Remove(curTarget);
+        CalculateIndex();
+        characters[curTarget].SetActive(false);
+    }
 }
+
